793cc66760d8ffc576189ed0d0287440ed13b9a0
TS <tinusunny04@gmail.com>
2026-02-26 21:31:10 +0530

    Ui(admin): implement quick access buttons in admin dashboard

--- src/Admin/pages/AdminLandingPage.jsx
+++ src/Admin/pages/AdminLandingPage.jsx
@@ -3,8 +3,11 @@ import AdminSidebar from '../components/AdminSidebar'
 import AdminFooter from '../components/AdminFooter'
 import { getUser } from '../../services/allAPIs'
 import { useNavigate } from 'react-router-dom'
-
-
+import { Box, Typography, Grid } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PeopleIcon from "@mui/icons-material/People";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import PaymentsIcon from "@mui/icons-material/Payments";
 function AdminLandingPage() {
 
  const navigate = useNavigate()
@@ -45,7 +48,137 @@ console.log(token);
     <>
  {token ?  <div>
       <AdminSidebar/>
-      <div>AdminLandingPage</div>
+
+
+{/* adimin homepage data  */}
+<Box>
+
+  {/* quick view */}
+
+    <Box sx={{ p: 4 }}>
+    <Box sx={{display:"flex", justifyContent:'space-evenly', alignItems:"center",flexDirection:{xs:'column',md:'row'},gap:{xs:5,md:0}}} >
+      
+      {/* Card 1 */}
+      <Grid item xs={12} sm={6} md={3}>
+        <Box
+          sx={{
+            background: "linear-gradient(135deg, #fbc02d, #f57f17)",
+            borderRadius: 3,
+            height:{xs:"100px",md:"120px"}, width:{xs:"80%",md:"100%"},
+            p: 3,
+            color: "#fff",
+            boxShadow: 4,
+            transition: "0.3s",
+            "&:hover": {
+              transform: "translateY(-5px)",
+              boxShadow: 8,
+            },
+          }}
+        >
+          <ShoppingCartIcon sx={{ fontSize:{xs:25,md:40} }} />
+          <Typography variant="h6" sx={{ mt: 1 }}>
+            Total Orders
+          </Typography>
+          <Typography sx={{ fontSize:{xs:25,md:30} }}  variant="h4" fontWeight="bold">
+            5
+          </Typography>
+        </Box>
+      </Grid>
+
+      {/* Card 2 */}
+      <Grid item xs={12} sm={6} md={3}>
+        <Box
+          sx={{
+            background: "linear-gradient(135deg, #4caf50, #2e7d32)",
+            borderRadius: 3,
+            height:{xs:"100px",md:"120px"}, width:{xs:"80%",md:"100%"},
+            p: 3,
+            color: "#fff",
+            boxShadow: 4,
+            transition: "0.3s",
+            "&:hover": {
+              transform: "translateY(-5px)",
+              boxShadow: 8,
+            },
+          }}
+        >
+          <PeopleIcon sx={{ fontSize:{xs:25,md:40} }}  />
+          <Typography variant="h6" sx={{ mt: 1 }}>
+            Total Users
+          </Typography>
+          <Typography sx={{ fontSize:{xs:25,md:30} }}  variant="h4" fontWeight="bold">
+            12
+          </Typography>
+        </Box>
+      </Grid>
+
+      {/* Card 3 */}
+      <Grid item xs={12} sm={6} md={3}>
+        <Box
+          sx={{
+            background: "linear-gradient(135deg, #2196f3, #1565c0)",
+            borderRadius: 3,
+            height:{xs:"100px",md:"120px"}, width:{xs:"80%",md:"100%"},
+            p: 3,
+            color: "#fff",
+            boxShadow: 4,
+            transition: "0.3s",
+            "&:hover": {
+              transform: "translateY(-5px)",
+              boxShadow: 8,
+            },
+          }}
+        >
+          <InventoryIcon sx={{ fontSize:{xs:25,md:40} }} />
+          <Typography variant="h6" sx={{ mt: 1 }}>
+            Products
+          </Typography>
+          <Typography sx={{ fontSize:{xs:25,md:30} }}  variant="h4" fontWeight="bold">
+            8
+          </Typography>
+        </Box>
+      </Grid>
+
+      {/* Card 4 */}
+      <Grid item xs={12} sm={6} md={3}>
+        <Box
+          sx={{
+            background: "linear-gradient(135deg, #e91e63, #ad1457)",
+            borderRadius: 3,
+            height:{xs:"100px",md:"120px"}, width:{xs:"80%",md:"100%"},
+            p: 3,
+            color: "#fff",
+            boxShadow: 4,
+            transition: "0.3s",
+            "&:hover": {
+              transform: "translateY(-5px)",
+              boxShadow: 8,
+            },
+          }}
+        >
+          <PaymentsIcon sx={{ fontSize:{xs:25,md:40} }}  />
+          <Typography variant="h6" sx={{ mt: 1 }}>
+            Enquiry
+          </Typography>
+          <Typography sx={{ fontSize:{xs:25,md:30} }}  variant="h4" fontWeight="bold">
+            25
+          </Typography>
+        </Box>
+      </Grid>
+
+    </Box>
+  </Box>
+
+
+
+</Box>
+
+
+
+      
+      
+      
+      
   <AdminFooter/>
    </div>:""}
 
