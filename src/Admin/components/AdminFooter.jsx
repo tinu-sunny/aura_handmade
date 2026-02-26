8671ce4890a3437887fd1ac7e09a2fbaae9bd3b0
TS <tinusunny04@gmail.com>
2026-02-26 20:52:38 +0530

    ui(admin): update admin footer layout

--- src/Admin/components/AdminFooter.jsx
+++ src/Admin/components/AdminFooter.jsx
@@ -1,8 +1,36 @@
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 function AdminFooter() {
   return (
-    <div>AdminFooter</div>
+   <>
+   
+<Box sx={{
+        backgroundColor:'#f5f5f5',
+          borderTop: "1px solid #ddd",
+        color: "#000",
+        mt: 5,
+        py: 3,
+        px: 2,
+      }}>
+ <Typography
+          variant="body2"
+          sx={{
+  color: "#222",
+  fontWeight: 600,
+  letterSpacing: 0.5,
+  textAlign: "center",
+}}
+
+        >
+          © 2026 HASTHKALA | Admin Dashboard. All Rights Reserved.
+        </Typography>
+  {/* <p>@2026 HASTHKALA.Admin Dashboard</p> */}
+
+
+</Box>
+  
+   </>
   )
 }
 
