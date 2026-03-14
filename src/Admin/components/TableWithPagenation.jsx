473b969315634ca5406c45516dbe3e698721f05f
TS <tinusunny04@gmail.com>
2026-03-14 22:15:03 +0530

    ui(admin): complete product add page design

---
+++ src/Admin/components/TableWithPagenation.jsx
@@ -0,0 +1,290 @@
+import * as React from "react";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Chip,
+  Box,
+  Dialog
+} from "@mui/material";
+
+const products = [
+  {
+    id: 1,
+    image: "https://cdn.shopify.com/app-store/listing_images/bba91cb747a794fddda7c4269609671c/promotional_image/CJrg-LWjjfsCEAE=.png?height=720&width=1280",
+    productName: "Handmade Clay Pot",
+    category: "Home Decor",
+    stockCount: 25,
+    price: 499,
+    status: "Available",
+  },
+  {
+    id: 2,
+    image: "https://cdn.shopify.com/app-store/listing_images/bba91cb747a794fddda7c4269609671c/promotional_image/CJrg-LWjjfsCEAE=.png?height=720&width=1280",
+    productName: "Wooden Wall Art",
+    category: "Wall Decor",
+    stockCount: 12,
+    price: 1299,
+    status: "Available",
+  },
+  {
+    id: 3,
+   image: "https://cdn.shopify.com/app-store/listing_images/bba91cb747a794fddda7c4269609671c/promotional_image/CJrg-LWjjfsCEAE=.png?height=720&width=1280",
+    productName: "Handcrafted Necklace",
+    category: "Jewelry",
+    stockCount: 0,
+    price: 899,
+    status: "Out of Stock",
+  },
+    {
+    id: 4,
+  image: "https://cdn.shopify.com/app-store/listing_images/bba91cb747a794fddda7c4269609671c/promotional_image/CJrg-LWjjfsCEAE=.png?height=720&width=1280",
+    productName: "Handcrafted Necklace",
+    category: "Jewelry",
+    stockCount: 0,
+    price: 899,
+    status: "Out of Stock",
+  },
+    {
+    id: 5,
+  image: "https://cdn.shopify.com/app-store/listing_images/bba91cb747a794fddda7c4269609671c/promotional_image/CJrg-LWjjfsCEAE=.png?height=720&width=1280",
+    productName: "Handcrafted Necklace",
+    category: "Jewelry",
+    stockCount: 0,
+    price: 899,
+    status: "Out of Stock",
+  },
+    {
+    id: 6,
+    image: "https://cdn.shopify.com/app-store/listing_images/bba91cb747a794fddda7c4269609671c/promotional_image/CJrg-LWjjfsCEAE=.png?height=720&width=1280",
+    productName: "Handmade Clay Pot",
+    category: "Home Decor",
+    stockCount: 25,
+    price: 499,
+    status: "Available",
+  },
+  {
+    id: 7,
+    image: "https://cdn.shopify.com/app-store/listing_images/bba91cb747a794fddda7c4269609671c/promotional_image/CJrg-LWjjfsCEAE=.png?height=720&width=1280",
+    productName: "Wooden Wall Art",
+    category: "Wall Decor",
+    stockCount: 12,
+    price: 1299,
+    status: "Available",
+  },
+  {
+    id: 8,
+     image: "https://cdn.shopify.com/app-store/listing_images/bba91cb747a794fddda7c4269609671c/promotional_image/CJrg-LWjjfsCEAE=.png?height=720&width=1280",
+    productName: "Handcrafted Necklace",
+    category: "Jewelry",
+    stockCount: 0,
+    price: 899,
+    status: "Out of Stock",
+  },
+    {
+    id: 9,
+     image: "https://cdn.shopify.com/app-store/listing_images/bba91cb747a794fddda7c4269609671c/promotional_image/CJrg-LWjjfsCEAE=.png?height=720&width=1280",
+    productName: "Handcrafted Necklace",
+    category: "Jewelry",
+    stockCount: 0,
+    price: 899,
+    status: "Out of Stock",
+  },
+    {
+    id: 10,
+     image: "https://cdn.shopify.com/app-store/listing_images/bba91cb747a794fddda7c4269609671c/promotional_image/CJrg-LWjjfsCEAE=.png?height=720&width=1280",
+    productName: "Handcrafted Necklace",
+    category: "Jewelry",
+    stockCount: 0,
+    price: 899,
+    status: "Out of Stock",
+  },
+];
+
+function TableWithPagination() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [openImage, setOpenImage] = React.useState(false);
+const [selectedImage, setSelectedImage] = React.useState("");
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const getStatusColor = (status) => {
+    if (status === "Available") return "success";
+    if (status === "Low Stock") return "warning";
+    return "error";
+  };
+
+  const handleImageOpen = (img) => {
+  setSelectedImage(img);
+  setOpenImage(true);
+};
+
+const handleImageClose = () => {
+  setOpenImage(false);
+};
+  return (
+    <Paper sx={{ width: "100%", borderRadius: 3, overflow: "hidden", boxShadow: 3 }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader>
+
+          {/* TABLE HEADER */}
+          <TableHead>
+            <TableRow sx={{ backgroundColor: "#f4b400" }}>
+              {[
+                "Image",
+                "Product Name",
+                "Category",
+                "ID",
+                "Stock",
+                "Price",
+                "Status",
+                "Action",
+              ].map((head) => (
+                <TableCell
+                  key={head}
+                  sx={{
+                    fontWeight: "bold",
+                    color: "#000",
+                    backgroundColor: "#f4b400",
+                    fontSize: "15px",
+                  }}
+                >
+                  {head}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+
+          {/* TABLE BODY */}
+          <TableBody>
+            {products
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((product) => (
+                <TableRow
+                  key={product.id}
+                  hover
+                  sx={{
+                    "&:hover": {
+                      backgroundColor: "#fff8e1",
+                    },
+                  }}
+                >
+                 <TableCell>
+  <img
+    src={product.image}
+    alt={product.productName}
+    onClick={() => handleImageOpen(product.image)}
+    style={{
+      width: 50,
+      height: 50,
+      borderRadius: 8,
+      objectFit: "cover",
+      cursor: "pointer",
+      transition: "0.3s",
+    }}
+    onMouseOver={(e) => (e.currentTarget.style.transform = "scale(1.1)")}
+    onMouseOut={(e) => (e.currentTarget.style.transform = "scale(1)")}
+  />
+</TableCell>
+
+                  <TableCell sx={{ fontWeight: 500 }}>
+                    {product.productName}
+                  </TableCell>
+
+                  <TableCell>{product.category}</TableCell>
+
+                  <TableCell>{product.id}</TableCell>
+
+                  <TableCell>{product.stockCount}</TableCell>
+
+                  <TableCell>₹{product.price}</TableCell>
+
+                  <TableCell>
+                    <Chip
+                      label={product.status}
+                      color={getStatusColor(product.status)}
+                      size="small"
+                    />
+                  </TableCell>
+
+                 <TableCell>
+  <Box
+    sx={{
+      display: "flex",
+      gap: 1,
+      alignItems: "center"
+    }}
+  >
+    <Box
+      sx={{
+        p: 1,
+        borderRadius: 2,
+        backgroundColor: "#e3f2fd",
+        cursor: "pointer",
+        "&:hover": { backgroundColor: "#bbdefb" }
+      }}
+    >
+      <span className="solar--eye-bold-duotone"></span>
+    </Box>
+
+    <Box
+      sx={{
+        p: 1,
+        borderRadius: 2,
+        backgroundColor: "#fff3e0",
+        cursor: "pointer",
+        "&:hover": { backgroundColor: "#ffe0b2" }
+      }}
+    >
+      <span className="material-symbols--edit-outline-rounded"></span>
+    </Box>
+  </Box>
+</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+<Dialog open={openImage} onClose={handleImageClose} maxWidth="sm">
+  <img
+    src={selectedImage}
+    alt="preview"
+    style={{
+      width: "100%",
+      height: "auto",
+      borderRadius: 8,
+    }}
+  />
+</Dialog>
+      {/* PAGINATION */}
+      <TablePagination
+        sx={{
+          backgroundColor: "#f4b400",
+          color: "#000",
+        }}
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={products.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
+
+export default TableWithPagination;
--- src/Admin/pages/AdminProductsManagePage.jsx
+++ src/Admin/pages/AdminProductsManagePage.jsx
@@ -1,12 +1,170 @@
 import React from 'react'
 import AdminSidebar from '../components/AdminSidebar'
 import AdminFooter from '../components/AdminFooter'
+import { Box, Button, Select, TextField, Typography,InputAdornment, MenuItem } from '@mui/material'
+import SearchIcon from "@mui/icons-material/Search";
+import TableWithPagenation from '../components/TableWithPagenation';
 
 function AdminProductsManagePage() {
   return (
   <>
   <AdminSidebar/>
-    <div>AdminProductsManagePage</div>
+    
+{/* body */}
+{/* top body box */}
+<Box sx={{m:5}}>
+   
+   {/* heading + add product button */}
+   <Box sx={{ display:'flex', justifyContent:"space-between", alignItems:"center" ,m:5}}>
+    {/* heading */}
+<Box>
+  <Typography
+    variant="h5"
+    sx={{
+      fontWeight: "bold",
+      fontSize: { xs: "24px", md: "32px" },
+      color: "#f4b400",
+      animation: "fadeSlide 1s ease-in-out",
+      "@keyframes fadeSlide": {
+        "0%": {
+          opacity: 0,
+          transform: "translateY(20px)",
+        },
+        "100%": {
+          opacity: 1,
+          transform: "translateY(0)",
+        },
+      },
+    }}
+  >
+    Products Management
+  </Typography>
+
+  <Typography
+    variant="body1"
+    sx={{
+      fontWeight: 500,
+      color: "#555",
+      mt: 0.5,
+      animation: "fadeSlide 1.4s ease-in-out",
+      "@keyframes fadeSlide": {
+        "0%": {
+          opacity: 0,
+          transform: "translateY(20px)",
+        },
+        "100%": {
+          opacity: 1,
+          transform: "translateY(0)",
+        },
+      },
+    }}
+  >
+    Monitor Your Handcrafted Inventory
+  </Typography>
+</Box>
+  {/* new product add button */}
+  <Box>
+    <Button variant='contained' sx={{backgroundColor:"#f4b400",color:"black",  animation: "fadeSlide 1.4s ease-in-out",
+      "@keyframes fadeSlide": {
+        "0%": {
+          opacity: 0,
+          transform: "translateY(20px)",
+        },
+        "100%": {
+          opacity: 1,
+          transform: "translateY(0)",
+        },
+      }}}>
+    <span class="material-symbols-light--add" ></span>  Add New Products
+    </Button>
+  </Box>
+   </Box>
+
+   {/* Search and sort  */}
+
+
+<Box
+  sx={{
+    display: "flex",
+    flexDirection: { xs: "column", md: "row" },
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: 3,
+    p: 2,
+    backgroundColor: "#fff",
+    borderRadius: 3,
+    boxShadow: 2
+  }}
+>
+
+  {/* 🔍 Search Section (70%) */}
+  <Box sx={{ width: { xs: "100%", md: "70%" } }}>
+    <TextField
+      fullWidth
+      placeholder="Search products..."
+      variant="outlined"
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon sx={{ color: "#777" }} />
+          </InputAdornment>
+        ),
+      }}
+      sx={{
+        backgroundColor: "#f9f9f9",
+        borderRadius: 2
+      }}
+    />
+  </Box>
+
+  {/* ⚙ Filters (30%) */}
+  <Box
+    sx={{
+      width: { xs: "100%", md: "30%" },
+      display: "flex",
+      gap: 2
+    }}
+  >
+    {/* Category */}
+    <Select
+      fullWidth
+      defaultValue=""
+      displayEmpty
+      sx={{ backgroundColor: "#f9f9f9", borderRadius: 2 }}
+    >
+      <MenuItem value="">Category</MenuItem>
+      <MenuItem value="decor">Home Decor</MenuItem>
+      <MenuItem value="jewelry">Jewelry</MenuItem>
+      <MenuItem value="art">Handcrafted Art</MenuItem>
+    </Select>
+
+    {/* Status */}
+    <Select
+      fullWidth
+      defaultValue=""
+      displayEmpty
+      sx={{ backgroundColor: "#f9f9f9", borderRadius: 2 }}
+    >
+      <MenuItem value="">Status</MenuItem>
+      <MenuItem value="available">Available</MenuItem>
+      <MenuItem value="outofstock">Out of Stock</MenuItem>
+    </Select>
+  </Box>
+
+</Box>
+
+{/* Table For product view  */}
+<Box sx={{mt:5}} >
+  <TableWithPagenation/>
+</Box>
+
+   {/* top box end */}
+</Box>
+
+
+
+
+
     <AdminFooter/>
     </>
  
